04aa6d26d87baba15d761181e01ee9fd03087650
Rajat Sharma <lunasunkaiser@gmail.com>
2019-01-27 23:14:09 +0530

    Updates

---
+++ precache-manifest.a79d2bebc2cd40b216677a8c0b5c9361.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "48cdf3f8cb5dab20d7edc7603205c10e",
+    "url": "/glowing-enigma/static/media/howto.48cdf3f8.gif"
+  },
+  {
+    "revision": "a6da6e3ca1cae81d0ada",
+    "url": "/glowing-enigma/static/js/runtime~main.a6da6e3c.js"
+  },
+  {
+    "revision": "068851c35a52d69a79a6",
+    "url": "/glowing-enigma/static/js/main.068851c3.chunk.js"
+  },
+  {
+    "revision": "3965ae390b82bfa570b3",
+    "url": "/glowing-enigma/static/js/1.3965ae39.chunk.js"
+  },
+  {
+    "revision": "068851c35a52d69a79a6",
+    "url": "/glowing-enigma/static/css/main.8a5e20f2.chunk.css"
+  },
+  {
+    "revision": "095debc7f80ad1e56c94f6603b29c288",
+    "url": "/glowing-enigma/index.html"
+  }
+];
